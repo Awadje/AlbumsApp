cb6553ebbd203464f70aac8d698085f62388127f
Awadje <michaelawad85@gmail.com>
2017-06-01 21:44:09 +0200

    render component

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactNative from 'react-native';
 
-
 const App = () => {
   return (
     <Text>Some Text</Text>
   );
 };
 
-// Render it to the device
+ReactNative.AppRegistry.registerComponent('albums', () => App);

cde78dfe6c4faa85d1db49327c3296435cc68204
Awadje <michaelawad85@gmail.com>
2017-06-02 12:14:11 +0200

    make a component

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text } from 'react-native';
 
 //make a component
-
+const Header = () => {
+  return <Text>Albums</Text>;
+};
 
 //make component available for other parts of the app

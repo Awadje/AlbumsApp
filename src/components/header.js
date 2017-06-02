5c84283aa5e7127d4b6917fa23ce6afe33816614
Awadje <michaelawad85@gmail.com>
2017-06-02 12:17:03 +0200

    make component available for other parts of the app

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Text } from 'react-native';
 
-//make a component
+//
 const Header = () => {
   return <Text>Albums</Text>;
 };
 
-//make component available for other parts of the app
+export default Header;

8f4cef7eb0e30e75669aab2fd76d6e4ba9c21bb8
Awadje <michaelawad85@gmail.com>
2017-06-05 15:22:49 +0200

    Button Styled

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 const Button = () => {
+  const { buttonStyle, textStyle } = styles;
+
   return (
-    <TouchableOpacity>
-      <Text>Click me!!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+      Click me!!
+      </Text>
     </TouchableOpacity>
   );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 18,
+    color: 'white',
+    alignSelf: 'center'
+  },
+
+  buttonStyle: {
+    flex: 1,
+    height: 36,
+    backgroundColor: '#007aff',
+    borderColor: '#007aff',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 10,
+    alignSelf: 'stretch',
+    justifyContent: 'center'
+  }
 };
 
 export default Button;
